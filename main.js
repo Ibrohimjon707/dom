96eead344a0221e15e848b393f42b5ddd5cf1217
Ibrohimjon707 <ibrohimjonathamjonov38@gmail.com>
2025-02-24 17:34:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let h2 = document.querySelector('.ism')
+let ism1 = prompt("Ism kiriting")
+h2.textContent = ism1
+
+
+h2.style.cssText = `
+height: 100vh;
+    width: 100%;
+    font-size: 100px;
+    display: flex;
+    align-items: center;
+justify-content: center;
+margin-top: -10px;
+margin-bottom: -30px;
+color: yellow;
+`
+h2.style.backgroundColor ='blue';
+
